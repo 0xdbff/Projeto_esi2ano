53f730ca2723d736e2fee29f68ed06570cb70b35
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2022-10-04 23:29:15 +0100

    update Maui template for client

---
+++ client/client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace client;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ client/client/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace client;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+

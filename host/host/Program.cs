c97c659559d2f51220dd6966c0091fa506ecb3f5
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2022-10-07 10:23:40 +0100

    update Host

--- host/host/Program.cs
+++ host/host/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("HI");
+    }
+}

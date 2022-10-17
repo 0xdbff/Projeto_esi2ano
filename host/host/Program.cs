2eb96390600d90d988b7c8c7fbfa63005a4ada26
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2022-10-17 17:09:51 +0100

    database connection beta

--- host/host/Program.cs
+++ host/host/Program.cs
@@ -1,6 +1,25 @@
-﻿
+﻿using Npgsql;
 
-internal class Program
+var connString = "Host=localhost:5432;Username=admin;Password=S$0m*Pa$$Wor|d!;Database=testdb";
+
+await using var conn = new NpgsqlConnection(connString);
+await conn.OpenAsync();
+
+// Insert some data
+await using (var cmd = new NpgsqlCommand("INSERT INTO test (name) VALUES ($1)", conn))
 {
-    private static void Main(string[] args) { System.Console.WriteLine("HI"); }
+    cmd.Parameters.AddWithValue("Test nas Bd");
+    await cmd.ExecuteNonQueryAsync();
+}
+
+Console.WriteLine($"{conn}");
+
+// Retrieve all rows
+await using (var cmd = new NpgsqlCommand("SELECT name FROM test", conn))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+    while (await reader.ReadAsync())
+    {
+        Console.WriteLine(reader.GetString(0));
+    }
 }
